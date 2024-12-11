d6d649e31fc860c4124cfa08dc41c97d7c664e75
Krystof Rehacek <krystofee@gmail.com>
2024-12-11 10:10:03 +0100

    wip

--- bin/retino-prettier.js
+++ bin/retino-prettier.js
@@ -1,20 +1,14 @@
 #!/usr/bin/env node
 
-const requiredVersion = 'v16.13.0';
-if (process.version !== requiredVersion) {
-  console.error(`Error: Node.js ${requiredVersion} is required. Current version: ${process.version}`);
-  process.exit(1);
-}
+(async () => {
+  const prettier = await import('prettier'); // Import Prettier dynamically
+  const args = [
+    '--config=./src/.prettierrc',
+    '--ignore-path=./src/.prettierignore',
+    '**/*.html',
+  ];
 
-const { spawnSync } = require('child_process');
-
-// Customize arguments as needed. For your case:
-const args = [
-  'prettier',
-  '--config=./src/.prettierrc',
-  '--ignore-path=./src/.prettierignore',
-  '**/*.html'
-];
-
-const result = spawnSync('npx', args, { stdio: 'inherit' });
-process.exit(result.status);
+  const { spawnSync } = await import('child_process');
+  const result = spawnSync('npx', ['prettier', ...args], { stdio: 'inherit' });
+  process.exit(result.status);
+})();
