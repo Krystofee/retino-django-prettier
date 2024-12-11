a93cc6d7037ffd39fb7978e05545583bd0529b3f
Krystof Rehacek <krystofee@gmail.com>
2024-12-11 10:41:40 +0100

    wip

--- bin/retino-prettier.js
+++ bin/retino-prettier.js
@@ -21,6 +21,12 @@ console.log('Running retino.prettier.js ...');
   // Show node location
   console.log('... node location:', process.execPath);
 
+  // Echo NODE_PATH
+  console.log('... NODE_PATH:', process.env.NODE_PATH);
+
+  console.log(require.resolve('prettier'));
+  console.log(require.resolve('prettier-plugin-django-alpine'));
+
   // Show available packages
   console.log('... available packages:', fs.readdirSync('./node_modules').join(', '));
 
