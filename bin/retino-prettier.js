b43b004565b537448960a05776fa66b706ff04d3
Krystof Rehacek <krystofee@gmail.com>
2024-12-11 10:14:22 +0100

    wip

--- bin/retino-prettier.js
+++ bin/retino-prettier.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 
+console.log('Running retino.prettier.js ...');
+
 (async () => {
+  console.log('... loading Prettier');
+
   const prettier = await import('prettier'); // Import Prettier dynamically
+
+  console.log('... loaded Prettier');
+
   const args = [
     '--config=./src/.prettierrc',
     '--ignore-path=./src/.prettierignore',
@@ -10,5 +17,10 @@
 
   const { spawnSync } = await import('child_process');
   const result = spawnSync('npx', ['prettier', ...args], { stdio: 'inherit' });
+
+  console.log('... Prettier finished');
+
   process.exit(result.status);
+
+  console.log('... exiting');
 })();
